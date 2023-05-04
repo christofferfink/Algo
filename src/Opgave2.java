36bf43880151c231c209a412a9181d36616f94c9
Christoffer Fink <christo.fink.1@gmail.com>
2023-05-04 11:07:43 +0200

    warshal

--- src/Opgave2.java
+++ src/Opgave2.java
@@ -1,70 +1,76 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Opgave2 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
 
         String[] input = reader.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
         int F = Integer.parseInt(input[2]);
 
-        // Initialiser adjacenslisten for tognetværket
-        ArrayList<int[]>[] adjList = new ArrayList[N];
+        // Initialize adjacency list for the train network
+        List<List<Edge>> graph = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            adjList[i] = new ArrayList<int[]>();
+            graph.add(new ArrayList<>());
         }
 
+        // Read train routes and update adjacency list
         for (int i = 0; i < M; i++) {
             input = reader.readLine().split(" ");
             int v = Integer.parseInt(input[0]);
             int u = Integer.parseInt(input[1]);
             int w = Integer.parseInt(input[2]);
-            adjList[v].add(new int[]{u, w});
-            adjList[u].add(new int[]{v, w});
+            graph.get(v).add(new Edge(u, w));
+            graph.get(u).add(new Edge(v, w));
         }
 
-        // Brug Dijkstra's algoritme til at beregne den korteste rejsetid mellem alle togstationer
-        int[][] trainDistances = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(trainDistances[i], Integer.MAX_VALUE);
-            trainDistances[i][i] = 0;
-            PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
-            pq.add(new int[]{i, 0});
-            while (!pq.isEmpty()) {
-                int[] curr = pq.poll();
-                int node = curr[0];
-                int dist = curr[1];
-                if (dist > trainDistances[i][node]) {
-                    continue;
-                }
-                for (int[] edge : adjList[node]) {
-                    int neighbor = edge[0];
-                    int distance = edge[1];
-                    int newDist = dist + distance;
-                    if (newDist < trainDistances[i][neighbor]) {
-                        trainDistances[i][neighbor] = newDist;
-                        pq.add(new int[]{neighbor, newDist});
-                    }
-                }
-            }
-        }
+        // Calculate shortest distances using Floyd-Warshall algorithm
+        int[][] dist = floydWarshall(graph);
 
-        // Test, om turen kan gennemføres inden for 2 timer
         for (int i = 0; i < F; i++) {
             input = reader.readLine().split(" ");
             int v = Integer.parseInt(input[0]);
             int u = Integer.parseInt(input[1]);
-            if (trainDistances[v][u] > 120) {
+            if (dist[v][u] > 120) {
                 System.out.println("keep");
             } else {
                 System.out.println("cancel");
             }
+        }
+    }
+
+    static int[][] floydWarshall(List<List<Edge>> graph) {
+        int N = graph.size();
+        int[][] dist = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+            dist[i][i] = 0;
+            for (Edge edge : graph.get(i)) {
+                dist[i][edge.dest] = edge.weight;
+            }
+        }
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+
+    static class Edge {
+        int dest;
+        int weight;
+
+        public Edge(int dest, int weight) {
+            this.dest = dest;
+            this.weight = weight;
         }
     }
 }
