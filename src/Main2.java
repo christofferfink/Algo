88251940fe30e1a8c83f11a54492d39892b8a589
Christoffer Fink <christo.fink.1@gmail.com>
2023-05-04 10:27:58 +0200

    dijsktra

--- src/Main2.java
+++ src/Main2.java
@@ -2,12 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class Main2 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
 
         String[] input = reader.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
@@ -24,35 +22,37 @@ public class Main2 {
             input = reader.readLine().split(" ");
             int v = Integer.parseInt(input[0]);
             int u = Integer.parseInt(input[1]);
-            int w = Integer.parseInt(input[2]);
-            adjList[v].add(new int[]{u, w});
-            adjList[u].add(new int[]{v, w});
+            int d = Integer.parseInt(input[2]);
+            adjList[v].add(new int[]{u, d});
+            adjList[u].add(new int[]{v, d});
         }
 
-        // Brug Dijkstra's algoritme til at beregne den korteste rejsetid mellem alle togstationer
+        // Brug Dijkstra's algoritme til at beregne den korteste afstand mellem alle togstationer
         int[][] trainDistances = new int[N][N];
         for (int i = 0; i < N; i++) {
             Arrays.fill(trainDistances[i], Integer.MAX_VALUE);
             trainDistances[i][i] = 0;
-            PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
-            pq.add(new int[]{i, 0});
-            while (!pq.isEmpty()) {
-                int[] curr = pq.poll();
-                int node = curr[0];
-                int dist = curr[1];
-                if (dist > trainDistances[i][node]) {
-                    continue;
+            int[] dist = new int[N];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[i] = 0;
+            boolean[] visited = new boolean[N];
+            for (int j = 0; j < N; j++) {
+                int u = -1;
+                for (int k = 0; k < N; k++) {
+                    if (!visited[k] && (u == -1 || dist[k] < dist[u])) {
+                        u = k;
+                    }
                 }
-                for (int[] edge : adjList[node]) {
-                    int neighbor = edge[0];
+                visited[u] = true;
+                for (int[] edge : adjList[u]) {
+                    int v = edge[0];
                     int distance = edge[1];
-                    int newDist = dist + distance;
-                    if (newDist < trainDistances[i][neighbor]) {
-                        trainDistances[i][neighbor] = newDist;
-                        pq.add(new int[]{neighbor, newDist});
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + distance < dist[v]) {
+                        dist[v] = dist[u] + distance;
                     }
                 }
             }
+            trainDistances[i] = dist;
         }
 
         // Test, om turen kan gennemføres inden for 2 timer
