50cd66c62e4044940dae2d21ae6bfa6fad3a8207
Christoffer Fink <christo.fink.1@gmail.com>
2023-05-04 11:05:40 +0200

    lavet om i dijsktra

--- src/Main2.java
+++ src/Main2.java
@@ -1,70 +1,100 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
 
 public class Main2 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = reader.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
-        int F = Integer.parseInt(input[2]);
+        int N = parseInt(input[0]);
+        int M = parseInt(input[1]);
+        int F = parseInt(input[2]);
 
-        // Initialiser adjacenslisten for tognetværket
-        ArrayList<int[]>[] adjList = new ArrayList[N];
+        // Initialize adjacency list for the train network
+        List<List<Edge>> graph = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            adjList[i] = new ArrayList<int[]>();
+            graph.add(new ArrayList<>());
         }
 
+        // Read train routes and update adjacency list
         for (int i = 0; i < M; i++) {
             input = reader.readLine().split(" ");
-            int v = Integer.parseInt(input[0]);
-            int u = Integer.parseInt(input[1]);
-            int d = Integer.parseInt(input[2]);
-            adjList[v].add(new int[]{u, d});
-            adjList[u].add(new int[]{v, d});
-        }
-
-        // Brug Dijkstra's algoritme til at beregne den korteste afstand mellem alle togstationer
-        int[][] trainDistances = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(trainDistances[i], Integer.MAX_VALUE);
-            trainDistances[i][i] = 0;
-            int[] dist = new int[N];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            dist[i] = 0;
-            boolean[] visited = new boolean[N];
-            for (int j = 0; j < N; j++) {
-                int u = -1;
-                for (int k = 0; k < N; k++) {
-                    if (!visited[k] && (u == -1 || dist[k] < dist[u])) {
-                        u = k;
-                    }
-                }
-                visited[u] = true;
-                for (int[] edge : adjList[u]) {
-                    int v = edge[0];
-                    int distance = edge[1];
-                    if (dist[u] != Integer.MAX_VALUE && dist[u] + distance < dist[v]) {
-                        dist[v] = dist[u] + distance;
-                    }
-                }
+            if (input.length == 3) {
+                int v = parseInt(input[0]);
+                int u = parseInt(input[1]);
+                int w = parseInt(input[2]);
+                graph.get(v).add(new Edge(u, w));
+                graph.get(u).add(new Edge(v, w));
+            } else {
+                System.err.println("Invalid input: " + Arrays.toString(input));
             }
-            trainDistances[i] = dist;
         }
 
-        // Test, om turen kan gennemføres inden for 2 timer
+        // Calculate shortest distances using Dijkstra's algorithm
+        int[][] dist = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            dist[i] = dijkstra(graph, i);
+        }
+
         for (int i = 0; i < F; i++) {
             input = reader.readLine().split(" ");
-            int v = Integer.parseInt(input[0]);
-            int u = Integer.parseInt(input[1]);
-            if (trainDistances[v][u] > 120) {
-                System.out.println("keep");
+            if (input.length == 2) {
+                int v = parseInt(input[0]);
+                int u = parseInt(input[1]);
+                if (dist[v][u] > 120) {
+                    System.out.println("keep");
+                } else {
+                    System.out.println("cancel");
+                }
             } else {
-                System.out.println("cancel");
+                System.err.println("Invalid input: " + Arrays.toString(input));
             }
+        }
+    }
+
+    static int[] dijkstra(List<List<Edge>> graph, int start) {
+        int[] dist = new int[graph.size()];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+
+        while (!pq.isEmpty()) {
+            Edge curr = pq.poll();
+            int node = curr.dest;
+            int weight = curr.weight;
+
+            if (weight > dist[node]) {
+                continue;
+            }
+
+            for (Edge next : graph.get(node)) {
+                int alt = dist[node] + next.weight;
+                if (alt < dist[next.dest]) {
+                    dist[next.dest] = alt;
+                    pq.offer(new Edge(next.dest, alt));
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int dest;
+        int weight;
+
+        public Edge(int dest, int weight) {
+            this.dest = dest;
+            this.weight = weight;
+        }
+
+        public int compareTo(Edge other) {
+            return Integer.compare(weight, other.weight);
         }
     }
 }
